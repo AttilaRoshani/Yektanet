5f157c38964a89899a0add8a3e5f263cde23d526
Attila Roshani <92844001+AttilaRoshani@users.noreply.github.com>
2024-02-23 00:15:28 +0330

    Create ad.py

---
+++ ad.py
@@ -0,0 +1,33 @@
+from base_model import BaseAdvertising
+
+class Ad(BaseAdvertising):
+    def __init__(self, id, title, img_url, link, advertiser):
+        super().__init__()
+        self.id = id
+        self.title = title
+        self.img_url = img_url
+        self.link = link
+        self.advertiser = advertiser
+
+    def describe_me(self):
+        print(f"This is an Ad with title {self.title}")
+        
+
+    def get_title(self):
+        return self.title
+    
+
+    def set_title(self, title):
+        self.title = title
+
+
+    def get_clicks(self):
+        return self.clicks
+    
+
+    def inc_clicks(self):
+        self.clicks+=1
+
+
+    def set_advertiser(self, advertiser):
+        self.advertiser = advertiser

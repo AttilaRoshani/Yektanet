0b18995461e91f0229e116dc7d1a5a802ff55559
Attila Roshani <92844001+AttilaRoshani@users.noreply.github.com>
2024-02-24 22:09:30 +0330

    Update my.sql

---
+++ SQL/my.sql
@@ -0,0 +1,27 @@
+
+CREATE DATABASE IF NOT EXISTS ADDATABASE;
+
+USE ADDATABASE;
+--drop table
+
+DROP TABLE IF EXISTS Ad;
+DROP TABLE IF EXISTS AdvertiserTable;
+-- adver.. table
+CREATE TABLE IF NOT EXISTS AdvertiserTable (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    views INT,
+    clicks INT,
+    name NVARCHAR(512)
+);
+
+-- Ad table
+CREATE TABLE IF NOT EXISTS Ad (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    clicks INT,
+    views INT,
+    title NVARCHAR(512),
+    imgUrl NVARCHAR(512),
+    link NVARCHAR(512),
+    advertiser_id INT,
+    FOREIGN KEY (advertiser_id) REFERENCES AdvertiserTable(id)
+);

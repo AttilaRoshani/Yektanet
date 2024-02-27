3a38e0448093427a2a6f3245c4273e00f27c1db8
Attila Roshani <92844001+AttilaRoshani@users.noreply.github.com>
2024-02-27 13:38:40 +0330

    Update my.sql

--- SQL/my.sql
+++ SQL/my.sql
@@ -19,9 +19,9 @@ CREATE TABLE IF NOT EXISTS Ad (
     id INT PRIMARY KEY AUTO_INCREMENT,
     clicks INT,
     views INT,
-    title NVARCHAR(512),
-    imgUrl NVARCHAR(512),
-    link NVARCHAR(512),
+    title VARCHAR(512),
+    imgUrl VARCHAR(512),
+    link VARCHAR(512),
     advertiser_id INT,
     FOREIGN KEY (advertiser_id) REFERENCES AdvertiserTable(id)
 );
